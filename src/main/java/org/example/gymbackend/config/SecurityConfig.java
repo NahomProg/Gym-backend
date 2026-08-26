a631ed7a4156fedda072d66bb46c8f6f6280554b
NahomProg <nahoma4333@gmail.com>
2026-08-26 04:06:07 -0900

    ..

--- src/main/java/org/example/gymbackend/config/SecurityConfig.java
+++ src/main/java/org/example/gymbackend/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package org.example.gymbackend.config;
 
+import org.example.gymbackend.security.filter.JwtAuthenticationFilter;
+import org.example.gymbackend.security.filter.RateLimitingFilter;
 import org.springframework.context.annotation.*;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
